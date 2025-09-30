8772bc87e1edc66f49bd21744bca8f21028a4b80
Larry Diego <larry_diego@hotmail.com>
2025-09-30 14:12:06 -0300

    aula: interagindo com listas finalizada

--- ContentContext/Career.cs
+++ ContentContext/Career.cs
@@ -2,5 +2,11 @@ namespace Balta.ContentContext;
 
 public class Career : Content
 {
-    public int Courses { get; set; }
+    public IList<CareerItem> Items { get; set; }
+    public int TotalCourses => Items.Count;
+    public Career(string title, string url)
+        : base(title, url)
+    {
+        Items = new List<CareerItem>();
+    }
 }
--- ContentContext/Course.cs
+++ ContentContext/Course.cs
@@ -1,30 +1,17 @@
+using Balta.ContentContext.Enums;
+
 namespace Balta.ContentContext;
 
 public class Course : Content
 {
     public string Tag { get; set; }
     public IList<Module> Modules { get; set; }
+    public int DurationInMinutes { get; set; }
+    public EContentLevel Level { get; set; }
 
-    public Course()
+    public Course(string title, string url)
+        : base(title, url)
     {
         Modules = new List<Module>();
     }
-}
-
-public class Module
-{
-    public int Order { get; set; }
-    public string Title { get; set; }
-    public IList<Lecture> Lectures { get; set; }
-
-    public Module()
-    {
-        Lectures = new List<Lecture>();
-    }
-}
-
-public class Lecture
-{
-    public int Order { get; set; }
-    public string Title { get; set; }
 }
